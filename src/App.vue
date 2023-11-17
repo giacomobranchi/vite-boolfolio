ec673f11fed4a8fffe829d8bc2628208ad20ed42
giacomobranchi <branchigiacomo28@gmail.com>
2023-11-17 17:03:17 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
+export default {
+  setup() {
+    return {};
+  },
+  components: { HelloWorld }
+}
+</script>
+    
+<template>
+  <div>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+
+<style lang="scss" scoped></style>
